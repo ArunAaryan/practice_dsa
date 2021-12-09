e8b2a243f09629f147967f614df4900be8e31b29
arunaaryan <rnyadav98@gmail.com>
2021-12-09 18:44:43 +0530

    day106

--- Playground.py
+++ Playground.py
@@ -1,17 +1,14 @@
-def printCurrentSong(distance, speed, songDuration):
-	time_in_minutes = distance / speed
-	current_song = 0
-	for idx, song in enumerate(songDuration):
-		current_song += 1
-		minutes = int(song.split(':')[0])
-		seconds = int(song.split(":")[1])
-		t_seconds = (minutes * 60) + seconds
-		time_in_minutes -= t_seconds 
-		if time_in_minutes <= 0:
-			print (current_song)
-	print( current_song)
-
-res = printCurrentSong(2000, 5, ['05:30', '02:04']) 
+def combinationSum4(nums, target):
+	nums, combs = sorted(nums), [1] + [0] * (target)
+	for i in range(target + 1):
+		for num in nums:
+			if i == target:
+				print('here')
+			if num  > i: break
+			if num == i: combs[i] += 1
+			if num  < i: combs[i] += combs[i - num]
+	print(combs)
+	return combs[target]
+res = combinationSum4([1,2,3], 4)
 print(res)
-
-
+	
